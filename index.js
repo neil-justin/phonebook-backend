bbab91f5408b3695b6096f7c211c5b7663dc824e
Neil <mallarineil9@gmail.com>
2024-06-21 20:16:37 +0800

    Streamline deployment

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
-// const cors = require('cors')
+const cors = require('cors')
 
 const app = express();
 
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'));
+app.use(express.static('dist'))
 
 let phonebook = [
     {
